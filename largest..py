e58e3b747df9683f73451ccf30d502a28f7fa9e1
Rahul831 <pandurahul393@gmail.com>
2025-07-04 20:18:16 +0530

    basic py problems

---
+++ largest..py
@@ -0,0 +1,9 @@
+x= int(input())
+y= int(input())
+z= int(input())
+if x>y>z:
+    print("x is greater")
+elif x<y>z:
+    print("y is greater")
+else:
+    print("z is greater")
---
+++ swapping.py
@@ -0,0 +1,7 @@
+x=int(input())
+y=int(input())
+print(x,y)
+z=x
+x=y
+y=z
+print(f"after swapping x ={x}, after swapping y ={y}")
